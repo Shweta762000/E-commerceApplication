a586f44d7632936a164b0cae46eb57743c2a4932
Shweta762000 <shwetaswami762@gmail.com>
2023-12-09 00:29:15 +0530

    Revert "Completed Junit Testing of User Module."
    This reverts commit 93ba776daeaac0dadcac0b2feea4b37229d4ddff.

--- src/test/java/com/lcwd/electronic/store/electronicStore/ElectronicStoreApplicationTests.java
+++ src/test/java/com/lcwd/electronic/store/electronicStore/ElectronicStoreApplicationTests.java
@@ -7,9 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ElectronicStoreApplicationTests {
 
 	@Test
-	void contextLoads()
-	{
-		System.out.println("Testing project..");
+	void contextLoads() {
 	}
 
 }
